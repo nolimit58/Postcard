a6f16a5d734957bbc19d66fbbc71c1c4e55ca38d
Frederic <fredlambert@live.ca>
2014-10-17 23:00:14 -0400

    Add text for name

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
-    
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,13 +30,20 @@ class ViewController: UIViewController {
     
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         //Code will evaluate when we press the button
-        // Adding a comment here to test commits    
+        // Adding a comment here to test commits
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor=UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor() //Change la couleur du texte
+        
         enterMessageTextField.text = ""
-        enterMessageTextField.resignFirstResponder()
+        enterMessageTextField.resignFirstResponder() //Fait disparaitre le keyboard
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         mailButton.setTitle("Courrier envoyé", forState: UIControlState.Normal)
         
